64d50919f2efe4f1fdd0057050461f7117940dba
ikwyl6 <ikwyl6@protonmail.com>
2019-10-29 22:52:06 -0300

    nitial commit for rss-checker.sql

---
+++ rss-checker.sql
@@ -0,0 +1,20 @@
+/* Run as root */
+CREATE USER rss_checker@localhost IDENTIFIED BY 'password';
+GRANT ALL ON rss_checker.* TO rss_checker@localhost;
+
+/* Run as rss_checker */
+CREATE TABLE feed (\
+	id INT PRIMARY KEY NOT NULL auto_increment, \
+	title VARCHAR(500), \
+	url VARCHAR(1000) NOT NULL, \
+	updated DATETIME NOT NULL);
+/*
+CREATE TABLE rss (\
+	id INT PRIMARY KEY NOT NULL auto_increment, \
+	feed_id INT NOT NULL,\
+	title VARCHAR(500),\
+	url VARCHAR(1000) NOT NULL,\
+	published DATETIME NOT NULL, \
+	FOREIGN KEY (feed_id) REFERENCES feed(id) ON DELETE CASCADE);
+*/
+
