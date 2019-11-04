995d9d589d624bacc451d062b8f44dec37511b71
ikwyl6 <ikwyl6@protonmail.com>
2019-11-04 00:36:56 -0400

    updated db table structure slightly with 'comments' field and removed the unnessary root part

--- rss-checker.sql
+++ rss-checker.sql
@@ -1,12 +1,14 @@
 /* Run as root */
-CREATE USER rss_checker@localhost IDENTIFIED BY 'password';
+/*CREATE USER rss_checker@localhost IDENTIFIED BY 'password';
 GRANT ALL ON rss_checker.* TO rss_checker@localhost;
+*/
 
 /* Run as rss_checker */
 CREATE TABLE feed (\
 	id INT PRIMARY KEY NOT NULL auto_increment, \
 	title VARCHAR(500), \
 	url VARCHAR(1000) NOT NULL, \
+	comments INT(1), \
 	updated DATETIME NOT NULL);
 /*
 CREATE TABLE rss (\
