d0c811d898cb407a68eca9f69c061bbcf334d47a
ikwyl6 <ikwyl6@protonmail.com>
2022-10-03 21:11:17 -0300

    Update some lines from python lint
    Removed some spaces after commas, some end of line spaces removed. Removed add_item as redundant.
    Added 'remove_feed' to align with command line option added

--- Database.py
+++ Database.py
@@ -19,7 +19,7 @@ class db:
         return self
 
     # called when you finish with a 'with' command
-    def __exit__(self,type,value,trackback):
+    def __exit__(self, type, value, trackback):
         self.cursor.connection.close()
 
     # get all groups
@@ -31,7 +31,7 @@ class db:
     def get_all_feeds(self, **kwargs):
         feed_id = kwargs.get('feed_id', None)
         group = kwargs.get('group', None)
-        if (feed_id): 
+        if (feed_id):
             sql = "SELECT * FROM " + self.feed_table + " WHERE id = '" + feed_id + "';"
         elif (group):
             sql = "SELECT * FROM " + self.feed_table + " ORDER BY group_id ASC;"
@@ -47,16 +47,16 @@ class db:
     def add_feed(self, gid, feed_title, feed_url, feed_ts):
         if (feed_title is None): feed_title = ""
         feed_title = feed_title.replace("'", "''")
-        sql ="INSERT INTO " + self.feed_table + \
+        sql = "INSERT INTO " + self.feed_table + \
             " (title,url,updated) VALUES ('" + feed_title + "', '" + \
-            feed_url + "', '" + feed_ts + "')" 
+            feed_url + "', '" + feed_ts + "')"
         if (gid):
-            sql ="INSERT INTO " + self.feed_table + \
+            sql = "INSERT INTO " + self.feed_table + \
                 " (group_id,title,url,updated) VALUES ('" + str(gid) + "', '" + \
-                feed_title + "', '" + feed_url + "', '" + feed_ts + "')" 
+                feed_title + "', '" + feed_url + "', '" + feed_ts + "')"
         self.cursor.execute(sql, )
         self.con.commit()
-   
+
     # Add group
     def add_group(self, group_name):
         group_name = group_name.replace("'", "''")
@@ -64,14 +64,15 @@ class db:
         self.cursor.execute(sql, )
         self.con.commit()
 
-#    def add_item(self,feed_id, item):
-#        sql = "INSERT INTO " + self.rss_table + " (feed_id, title, url, published) VALUES ('" \
-#                + str(feed_id) + "', '" + item[0] + "', '" + item[1] + "', '" + str(item[2]) + "');"
-#        self.cursor.execute(sql, )
-#        self.con.commit()
+    # Remove feed
+    def remove_feed(self, feed_id):
+        sql = "DELETE FROM " + self.feed_table + \
+                " WHERE id = '" + feed_id + "'"
+        self.cursor.execute(sql, )
+        self.con.commit()
 
     # Update feed timestamp
-    def update_feed_dt(self,feed_id, dt):
+    def update_feed_dt(self, feed_id, dt):
         sql = "UPDATE " + self.feed_table + " SET updated = '" + str(dt) + "' WHERE id = '" + str(feed_id) + "';"
         self.cursor.execute(sql, )
         self.con.commit()
